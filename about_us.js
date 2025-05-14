462bdcc836be20a41d0d4d21759e584251693e47
Carol Hooper <ashleymk03@gmail.com>
2025-05-14 18:37:16 -0400

    fixed slider

--- about_us.js
+++ about_us.js
@@ -1,18 +1,32 @@
 const api_key = "ztLX4os3JEp08mfa5b119ZTlMnLiu1pPuJLkZAOwgaWepW52gjl9Fu73";
-const container = document.getElementById('image-container');
 
-fetch('https://api.pexels.com/v1/search?query=airplane&per_page=10', {
-  headers: {
-    Authorization: api_key
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    data.photos.forEach(photo => {
-      const img = document.createElement('img');
-      img.src = photo.src.medium;
-      img.alt = "Airplane";
-      img.style.margin = "10px";
-      container.appendChild(img);
-    });
-})
+
+async function loadAirplaneImages() {
+  const response = await
+  fetch('https://api.pexels.com/v1/search?query=airplane&per_page=10', {
+    headers: {
+      Authorization: api_key
+    }
+  })
+  const data = await response.json();
+  const photos = data.photos;
+  const slider = document.getElementById('myslider');
+  slider.innerHTML = "";
+
+  photos.forEach(photo => {
+    const img = document.createElement('img');
+    img.src = photo.src.medium;
+    img.style.width = "100%";
+    img.style.height = "100%";
+    slider.appendChild(img);
+  });
+
+  simpleslider.getSlider({
+    container: slider,
+    duration: 1,
+    delay: 3
+  });
+}
+
+window.onload = loadAirplaneImages();
+
