9dda7f553d8879e3ee5148ac0f7a43f3a67f0ca7
Carol Hooper <ashleymk03@gmail.com>
2025-05-15 17:21:57 -0400

    added elements of db

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express()
+const port = 3000;
+app.use(express.static(__dirname + '/public'));
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
